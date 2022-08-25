b987b0e551ea8e343cb7cba2521368731569f1b6
EmersonMatthias <emersonkilon@gmail.com>
2022-08-25 16:42:20 -0300

    versao_chatfuncionando

--- batepapuol.js
+++ batepapuol.js
@@ -1,6 +1,12 @@
 let dados;
 let statuz;
 let nome;
+let nome_usuario;
+let aaa;
+let bbb = 0;
+let por_mensagens;
+let scrolla;
+
 
 /*FUNÇÃO ENTRAR*/ 
 const entrar = () => {
@@ -9,6 +15,7 @@ const entrar = () => {
 
     verifica_nome.addEventListener('click', () => {
         nome = {name: document.querySelector('.tela_entrada').querySelector('input').value};
+        nome_usuario = document.querySelector('.tela_entrada').querySelector('input').value;
 
         axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nome)
             .then((m) => {
@@ -48,12 +55,121 @@ const entrar = () => {
 
 }
 
-/* FUNÇÃO QUE VERIFICA SE O USUÁRIO ESTÁ ONLINE */
+/* FUNÇÃO ENVIA MENSAGEM */
+const enviar = () => {
+    const botao_enviar = document.querySelector('footer').querySelector('button');
+    
+    botao_enviar.addEventListener('click', () => {
+        console.log('Clicou');
+        let mensagem = document.querySelector('footer').querySelector('input').value;
+        console.log(mensagem,nome_usuario);
+
+        let mensagem_servidor = {
+            from: nome_usuario,
+            to: "todos",
+            text: mensagem,
+            type: "message"
+        }
+
+        axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', mensagem_servidor)
+            .then((qas) => {
+                console.log('Mensagem enviada para o servidor');
+                console.log(qas.data);
+            })
+            .catch(() => {
+                console.log('Erro');
+            })
+    })
+
+
+
+}
+
+
+/*FUNÇÃO RECEBER/ATUALIZAR MENSAGENS*/
+const receber_mensagens = () => {
+
+    let requisitar_mensagens = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    por_mensagens = document.querySelector('.conversas');
+    
+
+   
+
+    let recebi_mensagens = () => {requisitar_mensagens.then((dados) => { 
+        aaa = dados.data.length;
+        console.log(dados.data[0]);
+
+        let now =  new Date().toLocaleTimeString();
+
+            
+        let limpa_mensagens = () => {
+            por_mensagens.innerHTML ="";
+    
+        }
+    
+        limpa_mensagens();
+
+            while (bbb < aaa){
+               
+            
+              
+           
+   
+            if (dados.data[bbb].type === "status"){
+                por_mensagens.innerHTML = por_mensagens.innerHTML + `
+                <div class="messagem">
+                    <span class="hour">(${now})</span>
+                    <span class="usarname">${dados.data[bbb].from}</span>
+                   
+                    ${dados.data[bbb].text}
+                  
+                 </div>`
+            }else if (dados.data[bbb].type === "message"){
+                por_mensagens.innerHTML = por_mensagens.innerHTML + `
+                <div class="messagem">
+                    <span class="hour">(${now})</span>
+                    <span class="usarname">${dados.data[bbb].from} para ${dados.data[bbb].to}: </span>
+                   
+                    ${dados.data[bbb].text}
+                  
+                 </div>`
+            }else if (dados.data[bbb].type === "private_message"){
+                por_mensagens.innerHTML = por_mensagens.innerHTML + `
+                <div class="messagem">
+                    <span class="hour">(${now}) </span>
+                    <span class="usarname">${dados.data[bbb].from} para ${dados.data[bbb].to}: </span>
+                   
+                    ${dados.data[bbb].text}
+                  
+                 </div>`
+            }
+            
+                bbb++;
+                scrolla = document.querySelector('.conversas').lastChild;
+                scrolla.scrollIntoView();
+            }
+
+            bbb = 0;
+
+            
+          
+            
+           
+    
+    })
+    }
+    recebi_mensagens();
+
+   
+}
 
 
 const load = () =>{
 
 entrar();
+enviar();
+setInterval(receber_mensagens, 2000);
+
 
 
 
